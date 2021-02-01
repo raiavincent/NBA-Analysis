791c8e2547aa8d8560a1eb0e02fbdc8e5fe80aba
Vincent Raia <vincentraia1@gmail.com>
2021-02-01 12:48:22 -0500

    create csv for r2

--- findRelationship.py
+++ findRelationship.py
@@ -2,12 +2,13 @@
 # possibly lambda function?
 # DONE: import the dataframe from createDF.py
 # DONE: need to make a date string to get the days dataframe
-# do I have to make a whole ass dataframe then do the math?
+# DONE: do I have to make a whole ass dataframe then do the math?
 
 import pandas as pd
 from datetime import datetime
 import scipy.stats
 from variable import r2
+import os
 
 startTime = datetime.now()
 
@@ -21,62 +22,39 @@ dataPath = (r'C:\Users\Vincent\Documents\GitHub'
             r'\Team Stats ' + dateString + '.csv')
 
 # get the data
+print('Gathering data.')
 league_data_df = pd.read_csv(dataPath)
+print('DONE: Data secured.')
 
 league_data_df = league_data_df[r2]
 
-# I think I want to drop 'string' formatted data to make it easier to 
-# complete the necessary math, but we can worry about that later, see how it
-# goes
 
-# scipy.stats.linregress(df1[['bins', 'one']].to_numpy()).rvalue ** 2
-# above is example code from SO
-
-decimals = 2
-print(round((scipy.stats.linregress
-       (league_data_df[['W', 'points']].to_numpy()).rvalue ** 2),decimals))
-
-
-# https://stackoverflow.com/questions/34896455/how-to-do-pearson-correlation
-# -of-selected-columns-of-a-pandas-data-frame
-# should be able to use the same thing for r2
-
-# realCorr = (league_numbers_df[league_numbers_df.columns[0:]].apply
-#             (lambda x: x.corr(league_numbers_df['W'])))
-# realCorr = (realCorr.sort_values(ascending=False))
-
-# series = pd.Series(data=league_data_df)
-
-# https://stackoverflow.com/questions/60124004/creating-dataframe-on-the-basis-of-r-squared-value
-
-# df2 = pd.DataFrame({
-#     'columns': df1.columns[1:],
-#     'r-square_with_bins': [
-#         scipy.stats.linregress(df1[['bins', col]].to_numpy()).rvalue ** 2
-#         for col in df1.columns[1:]
-#     ]
-# })
-
+print('Calculating and printing r2 to wins.')
 r2_df_dict = {}
 
+decimals = 2
+
 for col in league_data_df.columns[3:]:
-    r2 = round((scipy.stats.linregress(league_data_df[['W', col]].to_numpy()).rvalue ** 2),decimals)
+    r2 = round((scipy.stats.linregress(league_data_df[['W', col]].to_numpy())
+                .rvalue ** 2),decimals)
     print(col + ' ' + ' ' + str(r2))
     if col not in r2_df_dict.keys():
         r2_df_dict[col] = r2
+print('DONE: r2 calculated and printed.')
 
+print('Organizing dataframe.')
 r2_df = pd.DataFrame()
 r2_df = r2_df.append(r2_df_dict, ignore_index=True)
 r2_df = r2_df.transpose()
 r2_df = r2_df.rename(columns={0:'W'})
-r2_df =r2_df.sort_values('W',ascending=False)
+r2_df = r2_df.sort_values('W',ascending=False)
+print('DONE: Dataframe organized.')
 
-# r2_df = pd.DataFrame({
-#     'columns': league_data_df.columns[4:],
-#     'r-square_with_W': [
-#         scipy.stats.linregress(league_data_df[['W', col]].to_numpy()).rvalue ** 2
-#         for col in league_data_df.columns[1:]
-#     ]
-# })
+print('Saving dataframe to csv.')
+os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Excel Sheets')
+# os.chdir(r'/home/pi/Documents/Basketball-Analysis/Excel Sheets')
+dateString = datetime.strftime(datetime.now(), '%Y_%m_%d')
+r2_df.to_csv('R2 Measurements ' + dateString + '.csv',index=False)
+print('Saved to csv. Script complete.')
 
 print(datetime.now()-startTime)
