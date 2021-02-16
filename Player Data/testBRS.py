97750f4c6998bcb4294a18e1e1afc42cccdc4996
Vincent Raia <vincentraia1@gmail.com>
2021-02-16 08:32:37 -0500

    Update testBRS.py

--- Player Data/testBRS.py
+++ Player Data/testBRS.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 16 00:14:22 2021
-
-@author: Vincent
-"""
-
 from basketball_reference_scraper.players import get_stats, get_game_logs, get_player_headshot
 from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
 import pandas as pd
