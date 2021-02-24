ec0f6be1bb737347e74d59c36d3785ca67af7af2
Vincent Raia <vincentraia1@gmail.com>
2021-02-24 12:00:40 -0500

    document code and remove paths

--- masterFileUploadSkeleton.py
+++ masterFileUploadSkeleton.py
@@ -5,6 +5,8 @@ from datetime import date
 import calendar
 import os
 
+# code below sets up everything necessary for upload
+
 gauth = GoogleAuth()
 
 # Try to load saved client credentials
@@ -36,23 +38,28 @@ gauth.SaveCredentialsFile("mycreds.txt")
 
 drive = GoogleDrive(gauth)
 
+# get day of week for uploading to Sunday folder
 my_date = date.today()
 weekday = calendar.day_name[my_date.weekday()]
 weekday = weekday.lower()
 
+# get the a string of the current date for choosing the right file
 dateString = datetime.strftime(datetime.now(), '%Y_%m_%d')
 
+# current month to find the right folder to upload to
 currentMonth = datetime.now().strftime('%B')
 currentMonth = currentMonth.lower()
 
+# R2 file is just one folder, no months
 findRelationship = 'folderID'
 
+# strings of folder IDs for Sundays
 sundayFolderTeam = 'folderID'
-
 sundayFolderPlayerSeason = 'folderID'
-
 sundayFolderPlayerCareer = 'folderID'
 
+# these next 3 if statements match the current month with the right folder ID
+# TODO: wrap these all into one if statement, works fine but would be better
 if currentMonth == 'january':
     monthFolderTeam = 'folderID'
 elif currentMonth == 'february':
@@ -86,10 +93,13 @@ elif currentMonth == 'april':
 elif currentMonth == 'may':
     monthFolderPlayerCareer = 'folderID'
 
-os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Excel Sheets')
+# using the os module to change the directory for the right file
+os.chdir(r'path')
+# use the dateString to find the day's file
 todayFileFindRelationship = (f'R2 Measurements {dateString}.csv')
 todayFileTeamStats = (f'Team Stats {dateString}.csv')
 
+# upload file to the correct folder
 findRelationshipFile = drive.CreateFile({'parents': [{'id': findRelationship}]})
 findRelationshipFile.SetContentFile(todayFileFindRelationship)
 findRelationshipFile.Upload()
@@ -98,36 +108,43 @@ teamStatsFile = drive.CreateFile({'parents': [{'id': monthFolderTeam}]})
 teamStatsFile.SetContentFile(todayFileTeamStats)
 teamStatsFile.Upload()
 
-os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Player Data\Season')
+# using the os module to change the directory for the right file
+os.chdir(r'path')
+# use the dateString to find the day's file
 todayFilePlayerSeasonStats = (f'2021 Season Stats as of {dateString}.csv')
 
 playerSeasonStatsFile = drive.CreateFile({'parents': [{'id': monthFolderPlayerSeason}]})
 playerSeasonStatsFile.SetContentFile(todayFilePlayerSeasonStats)
 playerSeasonStatsFile.Upload()
 
-os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Player Data\Career')
+# using the os module to change the directory for the right file
+os.chdir(r'path')
+# use the dateString to find the day's file
 todayFilePlayerCareer = (f'Active Player Career Stats as of {dateString}.csv')
 
+# upload file to the correct folder
 todayPlayerCareerStats = drive.CreateFile({'parents': [{'id': monthFolderPlayerCareer}]})
 todayPlayerCareerStats.SetContentFile(todayFilePlayerCareer)
 todayPlayerCareerStats.Upload()
 
+# if day is sunday, upload to the sunday files, same code as directly aboce, 
+# but for Sunday files
 if weekday == 'sunday':
-    os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Excel Sheets')
+    os.chdir(r'path')
     todayFileTeamStats = (f'Team Stats {dateString}.csv')
     
     teamStatsFile = drive.CreateFile({'parents': [{'id': sundayFolderTeam}]})
     teamStatsFile.SetContentFile(todayFileTeamStats)
     teamStatsFile.Upload()
     
-    os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Player Data\Season')
+    os.chdir(r'path')
     todayFilePlayerSeasonStats = (f'2021 Season Stats as of {dateString}.csv')
     
     playerSeasonStatsFile = drive.CreateFile({'parents': [{'id': sundayFolderPlayerSeason}]})
     playerSeasonStatsFile.SetContentFile(todayFilePlayerSeasonStats)
     playerSeasonStatsFile.Upload()
     
-    os.chdir(r'C:\Users\Vincent\Documents\GitHub\Basketball-Analysis\Player Data\Career')
+    os.chdir(r'path')
     todayFilePlayerCareer = (f'Active Player Career Stats as of {dateString}.csv')
     
     todayPlayerCareerStats = drive.CreateFile({'parents': [{'id': sundayFolderPlayerCareer}]})
@@ -135,5 +152,5 @@ if weekday == 'sunday':
     todayPlayerCareerStats.Upload()
     
 else:
-    print('It is not sunday.')
+    print('It is not Sunday.')
         
